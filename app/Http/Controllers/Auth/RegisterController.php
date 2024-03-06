1505322e08b11bb1f63a95c69b9cd0cd256cfac1
Diego <diegotorresmijarra@gmail.com>
2024-03-06 16:03:57 +0100

    email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\WelcomeEmail;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -63,10 +65,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        Mail::to($user->email)->send(new WelcomeEmail($user));
+
+        return $user;
     }
 }
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenido a Tu Aplicación</title>
+    </head>
+    <body>
+        <h1>Bienvenido, {{ $user->name }}!</h1>
+        <p>Gracias por registrarte en nuestra aplicación.</p>
+    </body>
+</html>
